59170d3e5565ad45db5f8b63dca0391005a357fa
Ted <ted_cc@hotmail.com>
2017-03-10 16:45:07 -0400

    Registro

--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'nombre', 'apellidos', 'celular', 'email', 'password', 'pais', 'ciudad'
     ];
 
     /**
